f46e905010af03f618314fdf82f7e32bedd3b96a
qwetroman <you@example.com>
2022-01-17 22:05:17 +0000

    Nearly full done

--- client/client.c
+++ client/client.c
@@ -8,13 +8,10 @@
 #include <unistd.h> // for close
 #include <pthread.h>
 #define MAX 80
-
 #include "../server/config.h"
 #include "../server/mail.h"
 #include "../server/server_base.c"
 #include "../server/user.h"
-
-//    Mail mail = {.from = "", .to = "", .topic = "", .text = ""};
 
 void create_user(int sockfd)
 {
@@ -32,45 +29,14 @@ void create_user(int sockfd)
         while ((buff_username[n++] = getchar()) != '\n');
         strcpy(user.username, buff_username);
         printf("Enter the password : ");
-        n = 0;
-        while ((buff_password[n++] = getchar()) != '\n');
-        strcpy(user.password, buff_password);  
-        send(sockfd, &user, sizeof(user), 0);
-  	recv(sockfd, &feedback, sizeof(feedback), 0);
-  	printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-}
-
-void login_for_user(int sockfd)
-{
-
-    User user = {.id = 2, .username="", .password=""};
-    Feedback feedback;
-    char buff_username[MAX];
-    char buff_password[MAX];
-    int n;
-    int z;
-        bzero(buff_username, sizeof(buff_username));
-        bzero(buff_password, sizeof(buff_password));
-        printf("Enter the username : ");
-        n = 0;
-        while ((buff_username[n++] = getchar()) != '\n');
-        strcpy(user.username, buff_username);
-        printf("Enter the password : ");
         z = 0;
         while ((buff_password[z++] = getchar()) != '\n');
         strcpy(user.password, buff_password);  
         send(sockfd, &user, sizeof(user), 0);
   	recv(sockfd, &feedback, sizeof(feedback), 0);
   	printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-  	if (feedback.feedback==0){
-  	return buff_username;
-  	}
-  	else{
-  	return 0;
-  	}
 }
-
-void send_message_user(int sockfd,char username)
+void send_message_user(int sockfd,User user)
 {
 
     Mail mail = {.from = "", .to = "", .topic = "", .text = ""};
@@ -81,7 +47,78 @@ void send_message_user(int sockfd,char username)
     int n;
     int z;
     int y;
-    strcpy(mail.from,username);
+    strcpy(mail.from,user.username);
+        bzero(buff_to, sizeof(buff_to));
+        bzero(buff_topic, sizeof(buff_topic));
+        bzero(buff_text, sizeof(buff_text));
+        
+        printf("Enter the receiver of mail : ");
+        n = 0;
+        while ((buff_to[n++] = getchar()) != '\n');
+        strcpy(mail.to, buff_to);
+        printf("Enter the topic : ");
+        z = 0;
+        while ((buff_topic[z++] = getchar()) != '\n');
+        strcpy(mail.topic, buff_topic);
+        printf("Enter the text : ");
+        y = 0;
+        while ((buff_text[y++] = getchar()) != '\n');
+        strcpy(mail.text, buff_text);    
+        send(sockfd, &mail, sizeof(mail), 0);
+        recv(sockfd, &feedback, sizeof(feedback), 0);
+        printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
+        
+}
+
+void get_message_user(int sockfd,User user)
+{
+	Feedback feedback;
+	char message=user.username;
+	if(send(sockfd, &message, sizeof(message), 0)>0)
+    	printf("sent\n");
+	recv(sockfd, &feedback, sizeof(feedback), 0);
+  	printf("Feedback %d %s\n", feedback.feedback, feedback.message);
+  	if (feedback.feedback == 0){
+    		Mail mail_rcv = {.to="a"};
+    		printf("Reading mails...\n");
+
+    		while(strcmp(mail_rcv.to, "STOP") != 0){
+      			if (recv(sockfd, &mail_rcv, sizeof(mail_rcv), 0) > 0){
+        			printf("%s\n", mail_rcv.topic);
+      }
+      else{
+        printf("Received nothing\n");
+      }
+    }
+    }
+    else{
+    	printf("Nothing\n");
+    }
+        
+}
+
+
+void logout_for_user(int sockfd,User user)
+{
+    Feedback feedback;
+    user.id=3;
+    send(sockfd, &user, sizeof(user), 0);
+    recv(sockfd, &feedback, sizeof(feedback), 0);
+    printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
+}
+
+
+void login_for_user(int sockfd, int sckfd,int scfd)
+{
+
+    User user = {.id = 2, .username="", .password=""};
+    Feedback feedback;
+    char buff_username[MAX];
+    char buff_password[MAX];
+    char mode[MAX];
+    int n;
+    int z;
+    int y;
         bzero(buff_username, sizeof(buff_username));
         bzero(buff_password, sizeof(buff_password));
         printf("Enter the username : ");
@@ -96,12 +133,33 @@ void send_message_user(int sockfd,char username)
   	recv(sockfd, &feedback, sizeof(feedback), 0);
   	printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
   	if (feedback.feedback==0){
-  	return buff_username;
+  	printf("Thank you for login\n");
+  	for(;;){
+  	printf("Choose what you want to get\n");
+  	y = 0;
+        while ((mode[y++] = getchar()) != '\n');
+  	if ((strncmp(mode, "mail", 4)) == 0) {
+            printf("Give data for mail \n");
+            send_message_user(scfd,user);
+        }
+        else if ((strncmp(mode, "read", 4)) == 0) {
+            printf("Getting data \n");
+            get_message_user(sckfd,user);
+        }
+        else if ((strncmp(mode, "logout", 6)) == 0) {
+            printf("Loging out \n");
+            logout_for_user(sockfd,user);
+            break;
+        }
+  	
+  	}
   	}
   	else{
-  	return 0;
+  	printf("Try again or choose another option");
   	}
-}
+  	}
+
+
 
 
 
@@ -124,78 +182,6 @@ int main(){
 
   connect(server_in_socket, (struct sockaddr *) &server_addr, sizeof server_addr);
 
-  // create user
-//  send(server_out_user_socket, &user, sizeof(user), 0);
-  //recv(server_out_user_socket, &feedback, sizeof(feedback), 0);
-  //printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-
-  // create user
-  //user.id = 1;
-  //strcpy(user.username, "dan");
-  //send(server_out_user_socket, &user, sizeof(user), 0);
-  //recv(server_out_user_socket, &feedback, sizeof(feedback), 0);
-  //printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-
-
-  // login successful
-  //user.id = 2;
-  //strcpy(user.username, "anna");
-  //strcpy(user.password, "qwerty");
-  //send(server_out_user_socket, &user, sizeof(user), 0);
-  //recv(server_out_user_socket, &feedback, sizeof(feedback), 0);
-  //printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-
-  // login successful
-  //user.id = 2;
-  //strcpy(user.username, "dan");
-  //send(server_out_user_socket, &user, sizeof(user), 0);
-  //recv(server_out_user_socket, &feedback, sizeof(feedback), 0);
-  //printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-
-  //send mail - successful
-  //send(server_out_mail_socket, &mail, sizeof(mail), 0);
-  //recv(server_out_mail_socket, &feedback, sizeof(feedback), 0);
-  //printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-
-
-  //char message[] = "dan";
-
-  //if(send(server_in_socket, &message, sizeof(message), 0)>0)
-    //printf("sent\n");
-
-  //recv(server_in_socket, &feedback, sizeof(feedback), 0);
-  //printf("Feedback %d %s\n", feedback.feedback, feedback.message);
-
-  //if (feedback.feedback == 0){
-    //Mail mail_rcv = {.to="a"};
-    //printf("Reading mails...\n");
-
-    //while(strcmp(mail_rcv.to, "STOP") != 0){
-      //if (recv(server_in_socket, &mail_rcv, sizeof(mail_rcv), 0) > 0){
-        //printf("%s\n", mail_rcv.topic);
-     // }
-      //else{
-        //printf("Received nothing\n");
-        //break;
-      //}
-    //}
-  //}
-
-
-  //logout
-  //user.id = 3;
-  //strcpy(user.username, "anna");
-  //send(server_out_user_socket, &user, sizeof(user), 0);
-  //recv(server_out_user_socket, &feedback, sizeof(feedback), 0);
-  //printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-
-  //logout
-  //user.id = 3;
-  //strcpy(user.username, "dan");
-  //send(server_out_user_socket, &user, sizeof(user), 0);
-  //recv(server_out_user_socket, &feedback, sizeof(feedback), 0);
- // printf("Feedback: %d %s\n", feedback.feedback, feedback.message);
-  char username[MAX];
   for(;;){
   	char mode[MAX];
   	int n;
@@ -208,9 +194,7 @@ int main(){
         }
         if ((strncmp(mode, "login", 5)) == 0) {
             printf("Give data for login \n");
-            login_for_user(server_out_user_socket);
-            username=login_for_user(server_out_user_socket);
-            printf(username);  
+            login_for_user(server_out_user_socket,server_in_socket,server_out_mail_socket);
         }
   	
   	
